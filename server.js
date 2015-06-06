e4c1462b05920d01fc89e0f3dd4ec267efab77a8
Dewitt Buckingham <dj729@comcast.net>
2015-06-06 13:59:53 -0700

    Post response
    Server accepts a post request and generates an email based upon the body
    content

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
+
+// Server config
 var express = require('express');
 var app = express();
+var config = require('./config.json');
 
-app.get('/', function (req, res) {
-    res.send('Hello something');
+// Middleware config
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+// Mail config
+var mail = require('nodemailer');
+var transporter = mail.createTransport(config);
+
+// Settings
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+
+app.post('/', function (req, res) {
+    transporter.sendMail({
+        from: config.auth.user,
+        to: req.body.to,
+        subject: 'AUTOMATED MESSAGE: ' + req.body.subject,
+        text: req.body.message
+    }, function ( err, data ) {
+        if ( err ) {
+            res.status(400).end();
+        } else {
+            res.status(200).end();
+        }
+    });
 });
 
 var server = app.listen(3000, function() {
-    console.log('Express running at %s:%s', server.address().host, server.address().port);
+    console.log('Express running on port %s', server.address().host, server.address().port);
 });
